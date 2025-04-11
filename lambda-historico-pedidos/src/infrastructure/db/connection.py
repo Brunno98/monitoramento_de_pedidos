c806bbe83def00d7a5dd6022bdfd002d756d2e43
Brunno Santana <brunnosantana98@gmail.com>
2025-04-10 22:08:35 -0300

    corrigindo pool de conexoes com o banco

--- lambda-historico-pedidos/src/infrastructure/db/connection.py
+++ lambda-historico-pedidos/src/infrastructure/db/connection.py
@@ -2,8 +2,11 @@ from psycopg_pool import ConnectionPool
 from infrastructure.settings import get_database_url
 
 # Criando o pool uma vez só
-db_pool = ConnectionPool(conninfo=get_database_url())
+db_pool = ConnectionPool(
+    min_size=1,
+    max_size=10,
+    conninfo=get_database_url())
 
 
 def get_connection():
-    return db_pool.getconn()
+    return db_pool.connection()
